9475f4954898b5ae44834ca2205ccbf0284a0b9b
rezaerlangga3840 <126259479+rezaerlangga3840@users.noreply.github.com>
2024-11-10 20:27:51 +0700

    add login function phase 1

--- resources/views/dashboard/pages/admin/sidebar/partials/dropdowntest.blade.php
+++ resources/views/dashboard/pages/admin/sidebar/partials/dropdowntest.blade.php
@@ -13,13 +13,13 @@
         <div class="collapse " id="vrExamples">
           <ul class="nav nav-sm flex-column">
             <li class="nav-item">
-              <a class="nav-link text-dark " href="../../pages/pages/vr/vr-default.html">
+              <a class="nav-link text-dark " href="#">
                 <span class="sidenav-mini-icon"> V </span>
                 <span class="sidenav-normal  ms-1  ps-1"> VR Default </span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link text-dark " href="../../pages/pages/vr/vr-info.html">
+              <a class="nav-link text-dark " href="#">
                 <span class="sidenav-mini-icon"> V </span>
                 <span class="sidenav-normal  ms-1  ps-1"> VR Info </span>
               </a>
@@ -28,37 +28,37 @@
         </div>
       </li>
       <li class="nav-item ">
-        <a class="nav-link text-dark " href="../../pages/pages/pricing-page.html">
+        <a class="nav-link text-dark " href="#">
           <span class="sidenav-mini-icon"> P </span>
           <span class="sidenav-normal  ms-1  ps-1"> Pricing Page </span>
         </a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link text-dark " href="../../pages/pages/rtl-page.html">
+        <a class="nav-link text-dark " href="#">
           <span class="sidenav-mini-icon"> R </span>
           <span class="sidenav-normal  ms-1  ps-1"> RTL </span>
         </a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link text-dark " href="../../pages/pages/widgets.html">
+        <a class="nav-link text-dark " href="#">
           <span class="sidenav-mini-icon"> W </span>
           <span class="sidenav-normal  ms-1  ps-1"> Widgets </span>
         </a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link text-dark " href="../../pages/pages/charts.html">
+        <a class="nav-link text-dark " href="#">
           <span class="sidenav-mini-icon"> C </span>
           <span class="sidenav-normal  ms-1  ps-1"> Charts </span>
         </a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link text-dark " href="../../pages/pages/sweet-alerts.html">
+        <a class="nav-link text-dark " href="#">
           <span class="sidenav-mini-icon"> S </span>
           <span class="sidenav-normal  ms-1  ps-1"> Sweet Alerts </span>
         </a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link text-dark " href="../../pages/pages/notifications.html">
+        <a class="nav-link text-dark " href="#">
           <span class="sidenav-mini-icon"> N </span>
           <span class="sidenav-normal  ms-1  ps-1"> Notifications </span>
         </a>
